023d3aa1bb821aa7a84f138e82113b6439faf2b8
broekens <joost.broekens@gmail.com>
2016-03-11 14:46:05 +0100

    goal likelyhood function binding addition
    Added the functionality to bind a goal likelyhood function to a goal, so
    that you can program more sophisticated AI to determine whether a goal
    likelyhood changed or not. This will be autoselected for goal likelyhood
    changes by gamaygdala when you appraise an event relevant for a goal.

--- js/Gamygdala.js
+++ js/Gamygdala.js
@@ -650,9 +650,6 @@ TUDelft.Gamygdala.Agent.prototype.appraise = function(belief){
 	this.gamygdalaInstance.appraise(belief, this);
 }
 
-TUDelft.Gamygdala.Agent.prototype.appraiseGoal = function(goalName, causalAgentName){
-	this.gamygdalaInstance.appraise(new TUDelft.Gamygdala.Belief(1, causalAgentName, [goalName], [1], false), this);
-}
 TUDelft.Gamygdala.Agent.prototype.updateEmotionalState = function(emotion){
 	for(var i = 0; i < this.internalState.length; i++){
         if(this.internalState[i].name === emotion.name){
@@ -959,17 +956,16 @@ TUDelft.Gamygdala.Emotion = function (name, intensity) {
 * @param {double} utility The utility of the goal
 * @param {boolean} [isMaintenanceGoal] Defines if the goal is a maintenance goal or not. The default is that the goal is an achievement goal, i.e., a goal that once it's likelihood reaches true (1) or false (-1) stays that way.
 */
-TUDelft.Gamygdala.Goal = function(name, utility, isMaintenanceGoal, likelyhoodFunction){
+TUDelft.Gamygdala.Goal = function(name, utility, isMaintenanceGoal){
     this.name = name;
     this.utility = utility;
     this.likelihood = 0.5; //The likelihood is unknown at the start so it starts in the middle between disconfirmed (0) and confirmed (1)
+	this.calculateLikelyhood=false; //This is set to false, in which case gamygdala assumes beliefs (events) will be used to calculate the goal likelyhood by calculateDeltaLikelihood method. If set to true, instead gamygdala assumes this property is function that calculates the likelyhood, so you can bind this as follows
+									//yourGoal.calculateLikelyhood=yourGoalLikelyhoodFunction.bind(yourGoal, [param1, param2, etc...]); This results in yourGoal's calculateLikelyhood property to set to a function that is bound to both this goal object AND the params you need, enabling it to be evaluated by a param free call. (in fact you make your function a method of this goal instance)
 	if (isMaintenanceGoal)
 		this.isMaintenanceGoal=isMaintenanceGoal; //There are maintenance and achievement goals. When an achievement goal is reached (or not), this is definite (e.g., to a the promotion or not). A maintenance goal can become true/false indefinitely (e.g., to be well-fed)
 	else
 		this.isMaintenanceGoal=false;
-	if (likelyhoodFunction)
-		this.calculateLikelyhood=likelyhoodFunction.bind(this);
-	else
-		this.calculateLikelyhood=false;
+	
 }
 
